34a9ef24057e67c8e3512e930490ae77779aaeee
Aleksandra <aleks.mukhina@mail.ru>
2022-04-23 19:35:09 +0300

    fixed warnings

--- 1.py
+++ 1.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def constant_step(A, b, x, e, imax):
     """
     Функция для постоянного шага альфа
@@ -21,7 +24,7 @@ def constant_step(A, b, x, e, imax):
     r = b - A.dot(x)
     d = np.transpose(r).dot(r)
     d0 = d
-    while (i < imax) and (d > e**2 * d0):
+    while (i < imax) and (d > e ** 2 * d0):
         x = x + a * r
         r = b - A.dot(x)
         d = np.transpose(r).dot(r)
@@ -41,7 +44,7 @@ def variable_step(A, b, x, e, imax):
     r = b - A.dot(x)
     d = np.transpose(r).dot(r)
     d0 = d
-    while (i < imax) and (d > e**2 * d0):
+    while (i < imax) and (d > e ** 2 * d0):
         q = A.dot(r)
         a = d / (np.transpose(r).dot(q))
         x = x + a * r
@@ -51,38 +54,46 @@ def variable_step(A, b, x, e, imax):
     print(x, r, i)
 
 
-import numpy as np
-print("Enter '2' for 2x2 matrix, '3' for 3x3 and '4' for 4x4")
-matrix = int(input())
-if matrix == 2:
-    A = np.array([[2, 3], [4, 9]], dtype=float)
+def main():
+    matrix_a = np.array([[2, 3], [4, 9]], dtype=float)
     b = np.transpose(np.array([6, 15], dtype=float))
     x = np.transpose(np.array([1, 1], dtype=float))
     e = 0.01
     imax = 100
-    #x = (1.5, 1)
-elif matrix == 3:
-    A = np.array([[1, 2, 3], [3, 5, 7], [1, 3, 4]], dtype=float)
-    b = np.transpose(np.array([3, 0, 1], dtype=float))
-    x = np.transpose(np.array([-3, -12, 10], dtype=float))
-    e = 0.1
-    imax = 10000
-    #x = (-4, -13, 11)
-elif matrix == 4:
-    A = np.array([[1, -1, 3, 1], [4, -1, 5, 4], [2, -2, 4, 1], [1, -4, 5, -1]], dtype=float)
-    b = np.transpose(np.array([5, 4, 6, 3], dtype=float))
-    x = np.transpose(np.array([7, 15, 11, -15], dtype=float))
-    e = 0.1
-    imax = 100
-    #x = (9, 18, 10, -16)
-else:
-    print("You entered an invalid value")
-if matrix == 2 or matrix == 3 or matrix == 4:
-    print("Enter 1 for constant step alpha and 0 for variable")
-    choice = int(input())
-    if choice == 1:
-        constant_step(A, b, x, e, imax)
-    elif choice == 0:
-        variable_step(A, b, x, e, imax)
+    print("Enter '2' for 2x2 matrix, '3' for 3x3 and '4' for 4x4")
+    matrix = int(input())
+    if matrix == 2:
+        matrix_a = np.array([[2, 3], [4, 9]], dtype=float)
+        b = np.transpose(np.array([6, 15], dtype=float))
+        x = np.transpose(np.array([1, 1], dtype=float))
+        e = 0.01
+        imax = 100
+        # x = (1.5, 1)
+    elif matrix == 3:
+        matrix_a = np.array([[1, 2, 3], [3, 5, 7], [1, 3, 4]], dtype=float)
+        b = np.transpose(np.array([3, 0, 1], dtype=float))
+        x = np.transpose(np.array([-3, -12, 10], dtype=float))
+        e = 0.1
+        imax = 10000
+        # x = (-4, -13, 11)
+    elif matrix == 4:
+        matrix_a = np.array([[1, -1, 3, 1], [4, -1, 5, 4], [2, -2, 4, 1], [1, -4, 5, -1]], dtype=float)
+        b = np.transpose(np.array([5, 4, 6, 3], dtype=float))
+        x = np.transpose(np.array([7, 15, 11, -15], dtype=float))
+        e = 0.1
+        imax = 100
+        # x = (9, 18, 10, -16)
     else:
         print("You entered an invalid value")
+    if matrix == 2 or matrix == 3 or matrix == 4:
+        print("Enter 1 for constant step alpha and 0 for variable")
+        choice = int(input())
+        if choice == 1:
+            constant_step(matrix_a, b, x, e, imax)
+        elif choice == 0:
+            variable_step(matrix_a, b, x, e, imax)
+        else:
+            print("You entered an invalid value")
+
+
+main()
